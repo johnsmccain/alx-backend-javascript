0f9b75473fc136c07304098e98b34ceaaa443297
johnsmccain <johnsdanlami@gmail.com>
2023-02-21 06:39:11 +0100

    node basics

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -11,6 +11,9 @@ interface Directors extends Teacher {
   numberOfReports: number;
 }
 
+interface PrintTeacher {
+  (firstName: String, lastName: String): String
+}
 const director1: Directors = {
   firstName: 'John',
   lastName: 'Doe',
@@ -19,6 +22,40 @@ const director1: Directors = {
   numberOfReports: 17,
 };
 
+const printTeacherFunction: PrintTeacher (firstName: String, lastName: String) =>{
+  console.log(`${firstName.charAt(0)}. ${lastName}`)
+} 
+
 console.log(director1);
+
+// interface IStudentInterface<T> {
+//   constructor(firstName: T, lastName:T) {
+//   }
+//   workOnHomework(): String;
+//   displayName(): T
+// }
+
+// // class StudentInterface<T> implements IStudentInterface<T>{
+// //   constructor(firstName: String, lastName: String){
+
+// //   }
+// //   workOnHomework(): T;
+// //   displayName(): T
+// // }
+
+// class StudentClass implements IStudentInterface {
+//   // constructor(firstName: String, lastName:String) {
+//   //   this.firstName = firstName
+//   //   this.lastName = lastName
+//   // }
+
+//   workOnHomework() {
+//     return "Currently working";
+//   }
+
+//   displayName() {
+//     return this.firstName;
+//   }
+// }
 
 
